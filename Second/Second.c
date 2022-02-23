ceb7e2631af35366f2828c7f0e3e234362bdb0bf
MikhailShychko <mihalshichko@gmail.com>
2022-02-23 21:42:37 +0300

    commit selectd files

--- Second/Second.c
+++ Second/Second.c
@@ -1,8 +1,10 @@
 ﻿#include "stdbool.h";
 #include "stdio.h";
 #include "stdlib.h";
+#include "string.h";
 
-#define numOfChild 4
+#define numOfChild 2
+#define stringSize 128
 
 struct HealthyChild
 {
@@ -10,31 +12,27 @@ struct HealthyChild
 
 	char* lastDoctorName;
 
-	char* lastDoctorSurname;
-
-	bool isSick;
 };
 
 struct SickChild
 {
-	//char* lastSickness;
+	char lastSickness[stringSize];
 
-	//char* lastDoctorName;
+	char lastDoctorName[stringSize];
 
-	//char* lastDoctorSurname;
+	char hospitalNum[stringSize];
 
-	struct HealthyChild CommonInf;
+	char hospitalAdress[stringSize];
 
-	char* hospitalNum;
-
-	char* hospitalAdress;
-
-	char* currentDoctorSurname;
+	char currentDoctorSurname[stringSize];
 };
-
 
 struct Child
 {
+	bool isSick;
+
+	char* childSurname;
+
 	union Type
 	{
 		struct SickChild sickChild;
@@ -42,82 +40,155 @@ struct Child
 		struct HealthyChild healthyChild;
 
 	};
+
 };
 
-struct Child childBuilder() 
+char* getstr(char* st)
 {
-	struct Child child;
+	int i = 0;
+	while ((*(st + i++) = (char)getchar()) != '\n');
+	st[--i] = '\0';
+	return st;
+}
+
+char* getName(char str[]) 
+{
+	while (true)
+	{
+		rewind(stdin);
+		if (!getstr(str)) printf("\nErorr... Try again\n");
+
+		if (str[0] < 65 || str[0] > 90)
+		{
+			printf("\nError... surname starts with small letter\n");
+		}
+		else
+		{
+			break;
+		}
+	}
+
+	return str;
+}
+
+struct Child InputHealthy(struct Child* child)
+{
 	printf("Input last sickness: ");
-	child.healthyChild.lastSickness = malloc(sizeof(union Type));
-	while(!gets(child.healthyChild.lastSickness)) printf("\nErorr... Try again\n");
+	child->healthyChild.lastSickness = malloc(sizeof(char));
+	while (!getstr(child->healthyChild.lastSickness)) printf("\nErorr... Try again\n");
 
 	printf("Input last doctor name: ");
-	child.healthyChild.lastDoctorName = malloc(sizeof(union Type));
-	while(!gets(child.healthyChild.lastDoctorName)) printf("\nErorr... Try again\n");
+	child->healthyChild.lastDoctorName = malloc(sizeof(char));
+	while (!getName(child->healthyChild.lastDoctorName)) printf("\nErorr... Try again\n");
+}
 
-	printf("Input last doctor surname: ");
-	child.healthyChild.lastDoctorSurname = malloc(sizeof(union Type));
-	while(!gets(child.healthyChild.lastDoctorSurname)) printf("\nErorr... Try again\n");
+struct Child InputSick(struct Child* child)
+{
+	printf("Input last sickness: ");
+	while (!getstr(child->sickChild.lastSickness)) printf("\nErorr... Try again\n");
 
-	printf("Is child ill at the moment(y/n)?: ");
+	printf("Input last doctor name: ");
+	while (!getName(child->sickChild.lastDoctorName)) printf("\nErorr... Try again\n");
+
+	printf("Input hospital number: ");
+	while (!fgets(child->sickChild.hospitalNum, stringSize, stdin)) printf("\nErorr... Try again\n");
+
+	printf("Input hospital adress: ");
+	while (!fgets(child->sickChild.hospitalAdress, stringSize, stdin)) printf("\nErorr... Try again\n");
+
+	printf("Input current doctor surname: ");
+	while (!getName(child->sickChild.currentDoctorSurname)) printf("\nErorr... Try again\n");
+}
+
+struct Child* childBuilder() 
+{
+	struct Child *child;
+	child = malloc(sizeof(struct Child));
+	printf("Input childs surname: ");
+	child->childSurname = malloc(sizeof(char));
+	while(!getName(child->childSurname)) printf("\nErorr... Try again\n");
+
 	char sick;
-	while((sick = getchar()) != 'y' && (sick != 'n')) printf("\nErorr... Try again\n");
-	child.healthyChild.isSick = sick == 'y';
-
-	if (child.healthyChild.isSick) //truble
+	while (true) 
 	{
-		child.sickChild.CommonInf = child.healthyChild;
-
-		child.sickChild.hospitalNum = malloc(sizeof(union Type));
-		while(!gets(child.sickChild.hospitalNum)) printf("\nErorr... Try again\n");
-
-		child.sickChild.hospitalNum = malloc(sizeof(union Type));
-		printf("Input hospital number: ");
-		while(!gets(child.sickChild.hospitalNum)) printf("\nErorr... Try again\n");
-
-		printf("Input hospital adress: ");
-		child.sickChild.hospitalAdress = malloc(sizeof(union Type));
-		while(!gets(child.sickChild.hospitalAdress)) printf("\nErorr... Try again\n");
-
-		printf("Input current doctor surname: ");
-		child.sickChild.currentDoctorSurname = malloc(sizeof(union Type));
-		while(!gets(child.sickChild.currentDoctorSurname)) printf("\nErorr... Try again\n");
-
+		printf("Is child ill at the moment(y/n)?: ");
+		sick = getchar();
+		getchar();
+		if (sick != 'y' && sick != 'n') 
+		{
+			printf("\nError try again...\n");
+		}
+		else
+		{
+			child->isSick = sick == 'y';
+			break;
+		}
 	}
+
+	if (child->isSick)
+	{
+		InputSick(child);
+	}
+	else
+	{
+		InputHealthy(child);
+	}
+	
 	return child;
 
 }
 
-int main(int argc, char** argv)
+void outputChildrenByCondition(struct Child children[], char sickness[], int num)
 {
-	struct Child children[numOfChild];
-	for (int i = 0; i < numOfChild; i++)
+	for (int i = 0; i < num; i++)
 	{
-		children[i] = childBuilder();
-	}
-
-	for (int i = 0; i < numOfChild; i++) 
-	{            
-		for (int j = numOfChild - 1; j > i; j--)
-		{ 
-			if (children[j - 1].healthyChild.lastDoctorName[0] > children[j].healthyChild.lastDoctorName[0])
+		for (int j = num - 1; j > i; j--)
+		{
+			if (children[j - 1].childSurname[0] > children[j].childSurname[0])
 			{
 				struct Child temp = children[j - 1];
 				children[j - 1] = children[j];
 				children[j] = temp;
 			}
 		}
-
 	}
 
-	for (int i = 0; i < numOfChild; i++)
+	printf("\nNeeded childern:\n");
+	for (int i = 0; i < num; i++)
 	{
-		if (children[i].sickChild.CommonInf.lastSickness == argv[1]) 
+		if (children[i].isSick)
 		{
-			printf("%s", children[i].sickChild.CommonInf.lastSickness);
+			if (!strcmp(children[i].sickChild.lastSickness, sickness))
+			{
+				printf("Child surname: ");
+				puts(children[i].childSurname);
+				printf("\n");
+			}
+		}
+		else
+		{
+			if (!strcmp(children[i].healthyChild.lastSickness, sickness))
+			{
+				printf("Child surname: ");
+				puts(children[i].childSurname);
+				printf("\n");
+			}
 		}
 	}
-
+	
 }
 
-
+int main(int argc, char* argv[])
+{
+	int num;
+	printf("Num: \n");
+	while(!scanf_s("%d", &num)) printf("\nYou have to input int\n");
+	struct Child *children = malloc(num * sizeof(struct Child));
+	for (int i = 0; i < num; i++)
+	{
+		printf("#%d\n", i + 1);
+		children[i] = *childBuilder();
+	}
+	
+	outputChildrenByCondition(children, argv[1], num);	
+}
